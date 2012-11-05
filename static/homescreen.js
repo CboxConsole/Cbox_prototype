561080f862f16d67a80b4ed4d8a2e6c4633cfe28
ragingwind <ragingwind@gmail.com>
2012-11-05 15:16:27 +0900

    disable opacity animation

--- static/homescreen.js
+++ static/homescreen.js
@@ -1,43 +1,6 @@
-// var Box = Backbone.View.extend({
-// 	className:'box',
-// 	to:undefined,
-// 	initialize: function() {
-// 		this.on('keydown:up keydown:down keydown:right keydown:left', 
-// 			this.navigate, this);
-// 		this.to = {};
-// 	},
-// 	render: function() {
-// 		if (this.options.game.title)
-// 			this.$el.append('<div class="titlebar">' + this.options.game.title + '</div>');
 
-// 		this.$el.css('background-image', 'url("games/' + this.options.game.banner + '")');
-// 		return this;
-// 	},
-// 	navigate: function(v, e) {
-// 		if (this.to[e.keyid]) this.to[e.keyid].focusin();
-// 	}
-// });
-
-// var SideBox = Backbone.View.extend({
-// 	className:'side',
-// 	initialize: function() {
-// 		this.boxes = {
-// 			upper: new Box({id:'upper', game:this.options.games[0]}),
-// 			down: new Box({id:'down', game:this.options.games[1]})
-// 		}
-// 		this.on('keyevent:focusin', function() {
-// 			this.boxes.upper.focusin();
-// 		}, this)
-// 	},
-// 	render: function() {
-// 		_.each(this.boxes, function(p) {
-// 			p.on('keydown:enter', function() {
-// 				this.superview.trigger('box:select', p, p.options.game);
-// 			}, this);
-// 			this.add(p.render());}, this);
-// 		return this;
-// 	}
-// });
+// Cbox homescreen
+// @ragingwind
 
 var Slider = Backbone.View.extend({
 	className:'slider',
@@ -66,15 +29,15 @@ var Slider = Backbone.View.extend({
       }
 	},
 	render: function() {
-		var bg = 'url(' + this.options.game.banner + ')';
 		var $reflect = $('<canvas class="reflect"></canvas>');
-		var $img = $('<img class="banner"></img>');
-		$img.attr('src', this.options.game.banner);
+		var $img = $('<img class="banner" src="' + this.options.game.banner + '"></img>');
+		
 		this.$el.append($img);
 		this.$el.append($reflect[0]);
 
 		this.options.reflection = 0.5;
 		this.options.bgColor =  '#00F';
+		
 		var self = this;
 		$img.load(function() {
       self.reflect($reflect[0], {
@@ -84,15 +47,6 @@ var Slider = Backbone.View.extend({
       'width': 396, //this.$el.width(),
       'color': self.options.bgColor});
     });
-
-		
-	// 	setTimeout(function() {
-	// 		var c=document.getElementById("myCanvas2");
-	// 	var ctx=c.getContext("2d");
-	// 	console.log(ctx);
-	// 	ctx.fillRect(20,20,150,100);
-	// }, 100);
-		
 
 		return this;
 	}
@@ -134,12 +88,19 @@ var SliderView = Backbone.View.extend({
 			
 			this.top--;
 			self.animate = true;
-
-			$v.animate({'opacity':0.5}, {
-  			duration:150,
-  			step: function(now) {!(now > 0.5 && now < 0.6) || $v.remove()},
-				complete: function() {self.animate = false;}
-  		});
+			
+			// @TODO performance issue with sdk
+			if (true) {
+				$v.remove()
+				self.animate = false;
+			}
+			else {
+				$v.animate({'opacity':0.1}, {
+  				duration:150,
+  				step: function(now) {if (now > 0.5 && now < 0.6) $v.remove()},
+					complete: function() {self.animate = false;}
+  			});
+  		}
 		}
 	},
 	render: function() {
@@ -154,20 +115,8 @@ var SliderView = Backbone.View.extend({
 
 var Homescreen = Backbone.View.extend({
 	className:'homescreen',
-	boxes: undefined,
 	initialize: function() {
 		var games = this.options.games
-		this.boxes = undefined;
-
-		// this.boxes = {
-		// 	left: new SideBox({id:'left', games:[games[0], games[3]]}),
-		// 	center: new Box({id:'center', game:games[2]}),
-		// 	right: new SideBox({id:'right', games:[games[1], games[5]]})
-		// }
-
-		// this.boxes.center.on('keydown:enter', function() {
-		// 	this.trigger('box:select', this.boxes.center, this.boxes.center.options.game);
-		// }, this);
 	},
 	render: function() {
 		this.$el.append('<div id=android></div>');
@@ -176,35 +125,8 @@ var Homescreen = Backbone.View.extend({
 			views.push(new Slider({game:game}).render())
 		});
 
-		this.sliderview = new SliderView({views:views}).render();
-		
-
-		// this.focusBox(0);
-
-		// sub boxes added to boxes view
-			// _.each(this.boxes, function(p) {this.add(p.render());}, this);
-			
-			// // mapping for key navigation
-			// var center = this.boxes.center
-			// 	,	left = this.boxes.left.boxes
-			// 	,	right = this.boxes.right.boxes;
-
-			// left.upper.to.right = center;
-			// left.upper.to.down = left.down;
-			// left.down.to.right = center;
-			// left.down.to.up = left.upper;
-
-			// right.upper.to.left = center;
-			// right.upper.to.down = right.down;
-			// right.down.to.left = center;
-			// right.down.to.up = right.upper;
-
-			// center.to.left = this.boxes.left;
-			// center.to.right = this.boxes.right;
-			// center.focusin();
-
-			// this.$el.append('<div class="shadow"></div>')
-		this.$el.append(this.sliderview.$el);
+		this.sliderview = new SliderView({views:views}).render();		
+		this.add(this.sliderview);
 		this.sliderview.focusin();
 		return this;
 	}
