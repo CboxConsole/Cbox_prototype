fb6919c2f2f8cc77c2c77d651fe5309363bcc3eb
ragingwind <ragingwind@gmail.com>
2012-11-14 11:22:29 +0900

    move event code to homescreen.js

--- static/main.js
+++ static/main.js
@@ -1,13 +1,4 @@
 $(document).ready(function() {
-	// create a homescreen.
 	var hs = window.homescreen = new Homescreen({games:GameConfig});
-
-	// bind box selecting event to launch games.
-	hs.on('game:select', function(game) {
-		// create and show game screen with the game parameter.
-		var gl = new GameLauncher({game:game});
-		$('body').append(gl.render().$el);		
-	});
-
 	$('body').append(hs.render().$el);
 })
